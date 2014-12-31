736b831e06726208e92e7a6284ac0a61e9423869
M-Atsuhiko <is0042px@ed.ritsumei.ac.jp>
2014-12-31 16:05:58 +0900

    initial

---
+++ make_All_graphs.R
@@ -0,0 +1,6 @@
+Gausian_prefix <- "~/workspace/Gausian/Gausian_Result/"
+Tsuishi_prefix <- "~/workspace/Tsuishi/Tsuishi_Result/"
+
+#Fs
+Filename <- paste(Gausian_prefix,"ca_Rerative_Gaus_75_0_Fs.xdr",sep="")
+load(Filename)
